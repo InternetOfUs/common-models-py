74770be58010516c0506b053d14705e74c187744
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2020-05-06 16:41:22 +0200

    Added wrapper for the user profile endpoint, and added some custom exceptions for some of the cases in which the service API return a 404 error

--- src/wenet/common/interface/service_api.py
+++ src/wenet/common/interface/service_api.py
@@ -2,9 +2,11 @@ from typing import Optional
 
 import requests
 
+from wenet.common.interface.exceptions import TaskNotFound, UpdateMetadataError
 from wenet.common.model.task.transaction import TaskTransaction
 from wenet.common.model.user.authentication_account import WeNetUserWithAccounts
 from wenet.common.model.task.task import Task
+from wenet.common.model.user.user_profile import WeNetUserProfile
 
 
 class ServiceApiInterface:
@@ -46,17 +48,23 @@ class ServiceApiInterface:
         }
         req = requests.post(self.base_url + self.USER_ENDPOINT + "/account/metadata", json=payload)
         if req.status_code != 200:
-            raise ValueError(f"No WeNet user [{wenet_user_id}] associated with Telegram id [{telegram_id}]")
+            raise UpdateMetadataError(wenet_user_id, telegram_id)
 
     def create_task(self, task: Task):
         requests.post(self.base_url + self.TASK_ENDPOINT, json=task.to_repr())
 
     def get_task(self, task_id: str) -> Task:
         req = requests.get(self.base_url + self.TASK_ENDPOINT + '/%s' % task_id)
-        task = Task.from_repr(req.json(), task_id)
         if req.status_code == 404:
-            raise ValueError(f"Task [{task_id}] does not exist")
+            raise TaskNotFound(task_id)
+        task = Task.from_repr(req.json(), task_id)
         return task
 
     def create_task_transaction(self, transaction: TaskTransaction):
         requests.post(self.base_url + self.TASK_ENDPOINT + '/transaction', json=transaction.to_repr())
+
+    def get_user_profile(self, wenet_user_id: str) -> Optional[WeNetUserProfile]:
+        req = requests.get(self.base_url + self.USER_ENDPOINT + '/profile/%s' % wenet_user_id)
+        if req.status_code == 200:
+            return WeNetUserProfile.from_repr(req.json())
+        return None
