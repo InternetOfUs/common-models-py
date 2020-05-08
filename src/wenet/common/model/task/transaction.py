2aca1b9ba4f98f0d319c00db5dd9d100faa800fc
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2020-05-08 10:37:10 +0200

    Modified TaskTransaction type, according to the new specification. Also, forced checks on the transaction label given as input

--- src/wenet/common/model/task/transaction.py
+++ src/wenet/common/model/task/transaction.py
@@ -6,15 +6,20 @@ from wenet.common.model.task.task import TaskAttribute
 
 
 class TaskTransaction:
+    LABEL_VOLUNTEER_FOR_TASK = 'volunteerForTask'
+    LABEL_REFUSE_TASK = 'refuseTask'
+    LABEL_ACCEPT_VOLUNTEER = 'acceptVolunteer'
+    LABEL_REFUSE_VOLUNTEER = 'refuseVolunteer'
+    LABEL_TASK_COMPLETED = 'taskCompleted'
 
-    def __init__(self, task_id: str, type_id: str, attributes: Optional[List[TaskAttribute]]):
+    def __init__(self, task_id: str, label: str, attributes: Optional[List[TaskAttribute]]):
         self.task_id = task_id
-        self.type_id = type_id
+        self.label = label
         self.attributes = attributes
 
         if not isinstance(task_id, str):
             raise TypeError("Task id should be a string")
-        if not isinstance(type_id, str):
+        if not isinstance(label, str):
             raise TypeError("Type id should be a string")
 
         if self.attributes:
@@ -27,28 +32,39 @@ class TaskTransaction:
         else:
             self.attributes: List[TaskAttribute] = []
 
+        allowed_task_labels = [TaskTransaction.LABEL_ACCEPT_VOLUNTEER, TaskTransaction.LABEL_REFUSE_TASK,
+                               TaskTransaction.LABEL_TASK_COMPLETED, TaskTransaction.LABEL_REFUSE_VOLUNTEER,
+                               TaskTransaction.LABEL_VOLUNTEER_FOR_TASK]
+        if self.label not in allowed_task_labels:
+            raise ValueError(f"Label {label} not valid, it must be one of {allowed_task_labels}")
+
     def to_repr(self) -> dict:
         return {
             "taskId": self.task_id,
-            "typeId": self.type_id,
-            "attributes": list(x.to_repr() for x in self.attributes)
+            "label": self.label,
+            "attributes": {x.name: x.value for x in self.attributes}
         }
 
     @staticmethod
     def from_repr(raw_data: dict) -> TaskTransaction:
         return TaskTransaction(
             task_id=raw_data["taskId"],
-            type_id=raw_data["typeId"],
-            attributes=list(TaskAttribute.from_repr(x) for x in raw_data["attributes"]) if raw_data.get("attributes", None) else None
+            label=raw_data["label"],
+            attributes=[TaskAttribute(x, raw_data["attributes"][x]) for x in raw_data["attributes"]]
+            if raw_data.get("attributes", None) else None
         )
 
     def __eq__(self, o) -> bool:
         if not isinstance(o, TaskTransaction):
             return False
-        return self.task_id == o.task_id and self.type_id == o.type_id and self.attributes == o.attributes
+        return self.task_id == o.task_id and self.label == o.label and self.attributes == o.attributes
 
     def __repr__(self) -> str:
         return str(self.to_repr())
 
     def __str__(self) -> str:
         return self.__repr__()
+
+    def with_attribute(self, attribute: TaskAttribute) -> TaskTransaction:
+        self.attributes.append(attribute)
+        return self
--- test/wenet_test/service_api/test_task_transaction.py
+++ test/wenet_test/service_api/test_task_transaction.py
@@ -9,7 +9,7 @@ from wenet.common.model.task.transaction import TaskTransaction
 class TestTaskTransaction(TestCase):
 
     def test_repr(self):
-        task_transaction = TaskTransaction("taskId", "typeId", [TaskAttribute("name", "value")])
+        task_transaction = TaskTransaction("taskId", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name", "value")])
 
         from_repr = TaskTransaction.from_repr(task_transaction.to_repr())
 
@@ -17,15 +17,18 @@ class TestTaskTransaction(TestCase):
         self.assertEqual(task_transaction, from_repr)
 
     def test_equals(self):
-        task_transaction = TaskTransaction("taskId", "typeId", [TaskAttribute("name", "value")])
-        task_transaction1 = TaskTransaction("taskId", "typeId", [TaskAttribute("name", "value")])
-        task_transaction2 = TaskTransaction("taskId1", "typeId", [TaskAttribute("name", "value")])
-        task_transaction3 = TaskTransaction("taskId", "typeId1", [TaskAttribute("name", "value")])
-        task_transaction4 = TaskTransaction("taskId", "typeId", [TaskAttribute("name1", "value")])
-        task_transaction5 = TaskTransaction("taskId", "typeId", [TaskAttribute("name", "value"), TaskAttribute("name", "value")])
+        task_transaction = TaskTransaction("taskId", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name", "value")])
+        task_transaction1 = TaskTransaction("taskId", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name", "value")])
+        task_transaction2 = TaskTransaction("taskId1", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name", "value")])
+        task_transaction3 = TaskTransaction("taskId", TaskTransaction.LABEL_REFUSE_TASK, [TaskAttribute("name", "value")])
+        task_transaction4 = TaskTransaction("taskId", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name1", "value")])
+        task_transaction5 = TaskTransaction("taskId", TaskTransaction.LABEL_TASK_COMPLETED, [TaskAttribute("name", "value"), TaskAttribute("name", "value")])
         
         self.assertEqual(task_transaction, task_transaction1)
         self.assertNotEqual(task_transaction, task_transaction2)
         self.assertNotEqual(task_transaction, task_transaction3)
         self.assertNotEqual(task_transaction, task_transaction4)
         self.assertNotEqual(task_transaction, task_transaction5)
+
+    def test_wrong_label(self):
+        self.assertRaises(ValueError, TaskTransaction, "taskId", "typeId", [])
