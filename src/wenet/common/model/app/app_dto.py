ced3884ee96779238e29747a52836d9f9a8cc3d8
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2020-09-30 17:54:41 +0200

    Updated AppDTO and AppDeveloper model

--- src/wenet/common/model/app/app_dto.py
+++ src/wenet/common/model/app/app_dto.py
@@ -1,16 +1,24 @@
 from __future__ import absolute_import, annotations
 
+from enum import Enum
 from typing import Optional, List
 
 from wenet.common.model.app.platform_dto import PlatformDTO
 
 
+class AppStatus(Enum):
+
+    ACTIVE = 1
+    DEVELOPMENT = 0
+
+# TODO rename in app
 class AppDTO:
 
-    def __init__(self, creation_ts: Optional[int], last_update_ts: Optional[int], app_id: str, app_token: str, message_callback_url: Optional[str], metadata: Optional[dict]):
+    def __init__(self, creation_ts: Optional[int], last_update_ts: Optional[int], app_id: str, app_token: Optional[str], status: AppStatus, message_callback_url: Optional[str], metadata: Optional[dict]):
         self.creation_ts = creation_ts
         self.last_update_ts = last_update_ts
         self.app_id = app_id
+        self.status = status
         self.app_token = app_token
         self.message_callback_url = message_callback_url
         self.metadata = metadata
@@ -38,7 +46,8 @@ class AppDTO:
             "creationTs": self.creation_ts,
             "lastUpdateTs": self.last_update_ts,
             "appId": self.app_id,
-            "appToken": self.app_token,
+            "status": self.status.value,
+            "token": self.app_token,
             "messageCallbackUrl": self.message_callback_url,
             "metadata": self.metadata
         }
@@ -49,7 +58,8 @@ class AppDTO:
             creation_ts=raw_data.get("creationTs", None),
             last_update_ts=raw_data.get("lastUpdateTs", None),
             app_id=raw_data["appId"],
-            app_token=raw_data["appToken"],
+            status=AppStatus(raw_data["status"]),
+            app_token=raw_data.get("token", None),
             message_callback_url=raw_data.get("messageCallbackUrl", None),
             metadata=raw_data.get("metadata", None)
         )
@@ -65,3 +75,23 @@ class AppDTO:
 
     def __str__(self):
         return self.__repr__()
+
+
+class AppDeveloper:
+
+    def __init__(self, app_id: str, user_id: str):
+        self.app_id = app_id
+        self.user_id = user_id
+
+    def to_repr(self) -> dict:
+        return {
+            "appId": self.app_id,
+            "userId": self.user_id
+        }
+
+    @staticmethod
+    def from_repr(raw_data: dict) -> AppDeveloper:
+        return AppDeveloper(
+            app_id=raw_data["appId"],
+            user_id=raw_data["userId"]
+        )
