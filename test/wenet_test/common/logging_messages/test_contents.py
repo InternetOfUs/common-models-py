34d3a42e97d441ca232a6c7fb7483f693cb86d8d
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2021-01-13 15:37:26 +0100

    Added logging content models and tests

---
+++ test/wenet_test/common/logging_messages/test_contents.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+
+from wenet.common.model.logging_messages.contents import TextualContent, BaseContent, ActionContent, AttachmentContent,\
+    LocationContent, Card, CarouselContent
+
+
+class TestActionContent(TestCase):
+    def test_repr(self):
+        button = ActionContent("text", "payload")
+        self.assertEqual(button, BaseContent.from_repr(button.to_repr()))
+
+
+class TestTextualContent(TestCase):
+    def test_repr(self):
+        content = TextualContent("text")
+        content.with_button("button", "payload")
+        self.assertEqual(content, BaseContent.from_repr(content.to_repr()))
+
+
+class TestAttachmentContent(TestCase):
+    def test_repr(self):
+        content = AttachmentContent("uri", "alt")
+        content.with_button("button", "payload")
+        self.assertEqual(content, BaseContent.from_repr(content.to_repr()))
+
+
+class TestLocationContent(TestCase):
+    def test_repr(self):
+        content = LocationContent(12.4235, 33.0045)
+        content.with_button("button", "payload")
+        self.assertEqual(content, BaseContent.from_repr(content.to_repr()))
+
+
+class TestCard(TestCase):
+    def test_repr(self):
+        card = Card("title", "image url", "subtitle", {"action": "value"})
+        card.with_button("button", "payload")
+        self.assertEqual(card, Card.from_repr(card.to_repr()))
+
+
+class TestCarouselContent(TestCase):
+    def test_repr(self):
+        cards = [
+            Card("title", "image url", "subtitle", {"action": "value"})
+                .with_button("button", "payload")
+                .with_button("button", "payload"),
+            Card("title", "image url", "subtitle", {"action": "value"})
+                .with_button("button", "payload")
+                .with_button("button", "payload"),
+        ]
+        content = CarouselContent(cards)
+        self.assertEqual(content, BaseContent.from_repr(content.to_repr()))
