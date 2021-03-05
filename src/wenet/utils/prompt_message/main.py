6b48c6942f3fea24563dbcc81aaa1df18e93d75d
Samuel Bortolin <samuel.bortolin@u-hopper.com>
2021-03-05 18:08:17 +0100

    Added input sub-parser and refactoring

--- src/wenet/utils/prompt_message/main.py
+++ src/wenet/utils/prompt_message/main.py
@@ -9,6 +9,9 @@ from wenet.common.interface.hub import HubInterface
 from wenet.common.model.message.message import TextualMessage
 
 
+logger = logging.getLogger("wenet.utils.prompt_message")
+
+
 def message_for_user(app_id: str, receiver_id: str, text: str, app_callback: str, title: str = "") -> None:
     message = TextualMessage(
         app_id,
@@ -21,40 +24,47 @@ def message_for_user(app_id: str, receiver_id: str, text: str, app_callback: str
         }
     )
     response = requests.post(app_callback, json=message.to_repr())
-    logging.debug(response.status_code)
-    logging.debug(response.text)
+    logger.debug(response.status_code)
+    logger.debug(response.text)
 
 
 if __name__ == "__main__":
 
-    argParser = argparse.ArgumentParser(description="Prompt message - Send a message to the whole community of an app")
+    arg_parser = argparse.ArgumentParser(description="Prompt message - Send a message to a specific user or to the whole community of an app")
 
-    argParser.add_argument("-i", "--instance", type=str, default="https://wenet.u-hopper.com/dev", help="The target WeNet instance")
-    argParser.add_argument("-a", "--app_id", required=True, type=str, help="The target application")
+    arg_parser.add_argument("-i", "--instance", type=str, default="https://wenet.u-hopper.com/dev", help="The target WeNet instance")
+    arg_parser.add_argument("-a", "--app_id", required=True, type=str, help="The target application")
 
-    subParsers = argParser.add_subparsers(dest="subparser", help="Message source")
+    sub_parsers = arg_parser.add_subparsers(dest="subParser", help="Message source")
 
-    text_parser = subParsers.add_parser("text", help="Send a single text")
+    text_parser = sub_parsers.add_parser("text", help="Send a single text message")
     text_parser.add_argument("-t", "--text", required=True, type=str, help="The text for the message to send")
-    text_parser.add_argument("-ti", "--title", type=str, help="The title for the message to send")
+    text_parser.add_argument("-ti", "--title", type=str, default="", help="The title for the message to send")
     text_parser.add_argument("-u", "--user_id", type=str, help="The user to send the message to")
 
-    args = argParser.parse_args()
+    file_parser = sub_parsers.add_parser("input", help="Send the text message of the current day from an input csv/tsv file, it should have 2 columns, `Date` and `Message`")
+    file_parser.add_argument("-p", "--path", required=True, type=str, help="The path of the csv/tsv file")
+    file_parser.add_argument("-u", "--user_id", type=str, help="The user to send the message to")
 
-    if args.subparser == "text":
+    args = arg_parser.parse_args()
+
+    if args.subParser == "text":
 
         hub_interface = HubInterface(f"{args.instance}/hub/frontend")
         app_details = hub_interface.get_app_details(args.app_id)
 
         if args.user_id:
             # message for specific user
-            logging.debug(f"Publishing text [{args.text}] for user [{args.user_id}]")
+            logger.debug(f"Publishing text [{args.text}] for user [{args.user_id}]")
             message_for_user(args.app_id, args.user_id, args.text, app_details["messageCallbackUrl"], title=args.title)
         else:
             user_ids = hub_interface.get_user_ids_for_app(args.app_id)
             for user_id in user_ids:
-                logging.debug(f"Publishing text [{args.text}] for user [{user_id}]")
+                logger.debug(f"Publishing text [{args.text}] for user [{user_id}]")
                 message_for_user(args.app_id, user_id, args.text, app_details["messageCallbackUrl"], title=args.title)
 
+    elif args.subParser == "input":
+        logger.debug("do logic here")
+
     else:
-        logging.warning("You should choose the working mode [text]")
+        logger.warning("You should choose one of the following working modes [text, input]")
